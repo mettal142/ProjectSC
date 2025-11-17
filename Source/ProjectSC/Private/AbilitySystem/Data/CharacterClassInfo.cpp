706e1a4e359600aad4e167b8e9b378c589ff6c3f
mettal142 <40825944+mettal142@users.noreply.github.com>
2025-11-17 21:47:25 +0900

    Delete Source directory

--- Source/ProjectSC/Private/AbilitySystem/Data/CharacterClassInfo.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "AbilitySystem/Data/CharacterClassInfo.h"
-
-FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
-{
-	return CharacterClassInformation.FindChecked(CharacterClass);
-}
--- Source/ProjectSC/Private/SCTypes/SCStructTypes.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "SCTypes/SCStructTypes.h"
-#include "AbilitySystem/Abilities/SCGameplayAbility.h"
-
-bool FSCCharacterAbilitySet::IsValid() const
-{
-    return AbilityTag.IsValid() && AbilityToGrant;
-}
